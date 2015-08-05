e3599c7c350892960e7cbc7f212a5f7c8a6bc560
Julien Dubois <julien.dubois@gmail.com>
2015-08-05 22:29:19 +0200

    Added all the new sample projects

--- README.md
+++ README.md
@@ -3,20 +3,6 @@ JHipster automated samples
 
 This project is for generating the JHipster sample projects, and have the continuous integration automatically test them.
 
-The sample projects are included here as Git submodules.
+The sample projects are included as Git submodules.
 
-## Sample project
-
-_The basic JHipster sample project, with all default options_
-
-| Website       | Java build    | JavaScript build |
-| ------------- | ------------- | ------------- |
-| [jhipster/jhipster-sample-app](https://github.com/jhipster/jhipster-sample-app)  | [![Java Build Status](https://drone.io/github.com/jhipster/jhipster-sample-app/status.png)](https://drone.io/github.com/jhipster/jhipster-sample-app/latest)  | [![JavaScript Build Status](https://travis-ci.org/jhipster/jhipster-sample-app.svg?branch=master)](https://travis-ci.org/jhipster/jhipster-sample-app)  |
-
-## Gradle project
-
-_A standard JHipster sample project, with the Gradle build option_
-
-| Website       | Java build    | JavaScript build |
-| ------------- | ------------- | ------------- |
-| [jhipster/jhipster-sample-app-gradle](https://github.com/jhipster/jhipster-sample-app-gradle)  | [![Java Build Status](https://drone.io/github.com/jhipster/jhipster-sample-app-gradle/status.png)](https://drone.io/github.com/jhipster/jhipster-sample-app-gradle/latest)  | [![JavaScript Build Status](https://travis-ci.org/jhipster/jhipster-sample-app-gradle.svg?branch=master)](https://travis-ci.org/jhipster/jhipster-sample-app-gradle)  |
+More information, as well as the result of those tests, are available on our [build status page](https://jhipster.github.io/build_status.html).
--- generate_projects.sh
+++ generate_projects.sh
@@ -7,6 +7,9 @@ echo "current project: jhipster-sample-app"
 echo "---------------------------------------------"
 cd jhipster-sample-app
 yo jhipster --force
+yo jhipster:entity BankAccount --force
+yo jhipster:entity Label --force
+yo jhipster:entity Operation --force
 git add .
 git commit -m "automatic project update"
 git push
@@ -16,6 +19,161 @@ echo "---------------------------------------------"
 echo "current project: jhipster-sample-app-gradle"
 echo "---------------------------------------------"
 cd jhipster-sample-app-gradle
+yo jhipster --force
+yo jhipster:entity BankAccount --force
+yo jhipster:entity Label --force
+yo jhipster:entity Operation --force
+git add .
+git commit -m "automatic project update"
+git push
+cd ..
+
+echo "---------------------------------------------"
+echo "current project: jhipster-sample-app-java7"
+echo "---------------------------------------------"
+cd jhipster-sample-app-java7
+yo jhipster --force
+yo jhipster:entity BankAccount --force
+yo jhipster:entity Label --force
+yo jhipster:entity Operation --force
+git add .
+git commit -m "automatic project update"
+git push
+cd ..
+
+echo "---------------------------------------------"
+echo "current project: jhipster-sample-app-gulp"
+echo "---------------------------------------------"
+cd jhipster-sample-app-gulp
+yo jhipster --force
+yo jhipster:entity BankAccount --force
+yo jhipster:entity Label --force
+yo jhipster:entity Operation --force
+git add .
+git commit -m "automatic project update"
+git push
+cd ..
+
+echo "---------------------------------------------"
+echo "current project: jhipster-sample-app-mongodb"
+echo "---------------------------------------------"
+cd jhipster-sample-app-mongodb
+yo jhipster --force
+yo jhipster:entity BankAccount --force
+git add .
+git commit -m "automatic project update"
+git push
+cd ..
+
+echo "---------------------------------------------"
+echo "current project: jhipster-sample-app-cassandra"
+echo "---------------------------------------------"
+cd jhipster-sample-app-cassandra
+yo jhipster --force
+git add .
+git commit -m "automatic project update"
+git push
+cd ..
+
+echo "---------------------------------------------"
+echo "current project: jhipster-sample-dto"
+echo "---------------------------------------------"
+cd jhipster-sample-dto
+yo jhipster --force
+yo jhipster:entity BankAccount --force
+yo jhipster:entity Label --force
+yo jhipster:entity Operation --force
+git add .
+git commit -m "automatic project update"
+git push
+cd ..
+
+echo "---------------------------------------------"
+echo "current project: jhipster-sample-elasticsearch"
+echo "---------------------------------------------"
+cd jhipster-sample-elasticsearch
+yo jhipster --force
+yo jhipster:entity BankAccount --force
+yo jhipster:entity Label --force
+yo jhipster:entity Operation --force
+git add .
+git commit -m "automatic project update"
+git push
+cd ..
+
+echo "---------------------------------------------"
+echo "current project: jhipster-sample-hazelcast"
+echo "---------------------------------------------"
+cd jhipster-sample-hazelcast
+yo jhipster --force
+yo jhipster:entity BankAccount --force
+yo jhipster:entity Label --force
+yo jhipster:entity Operation --force
+git add .
+git commit -m "automatic project update"
+git push
+cd ..
+
+echo "---------------------------------------------"
+echo "current project: jhipster-sample-nocache"
+echo "---------------------------------------------"
+cd jhipster-sample-nocache
+yo jhipster --force
+yo jhipster:entity BankAccount --force
+yo jhipster:entity Label --force
+yo jhipster:entity Operation --force
+git add .
+git commit -m "automatic project update"
+git push
+cd ..
+
+echo "---------------------------------------------"
+echo "current project: jhipster-sample-app-compass"
+echo "---------------------------------------------"
+cd jhipster-sample-app-compass
+yo jhipster --force
+git add .
+git commit -m "automatic project update"
+git push
+cd ..
+
+echo "---------------------------------------------"
+echo "current project: jhipster-sample-noi18n"
+echo "---------------------------------------------"
+cd jhipster-sample-noi18n
+yo jhipster --force
+yo jhipster:entity BankAccount --force
+yo jhipster:entity Label --force
+yo jhipster:entity Operation --force
+git add .
+git commit -m "automatic project update"
+git push
+cd ..
+
+echo "---------------------------------------------"
+echo "current project: jhipster-sample-app-websocket"
+echo "---------------------------------------------"
+cd jhipster-sample-app-websocket
+yo jhipster --force
+git add .
+git commit -m "automatic project update"
+git push
+cd ..
+
+echo "---------------------------------------------"
+echo "current project: jhipster-sample-app-oauth2"
+echo "---------------------------------------------"
+cd jhipster-sample-app-oauth2
+yo jhipster --force
+git add .
+git commit -m "automatic project update"
+git push
+cd ..
+
+echo "---------------------------------------------"
+echo "current project: jhipster-sample-app-token"
+echo "---------------------------------------------"
+cd jhipster-sample-app-token
 yo jhipster --force
 git add .
 git commit -m "automatic project update"
