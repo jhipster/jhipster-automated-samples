0915eadf57e16a1a86ddb285f48b7d2c9acced39
Julien Dubois <julien.dubois@gmail.com>
2015-08-06 13:40:08 +0200

    added script to clone repos

---
+++ clone_samples.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+echo "Cloning JHipster sample projects"
+cd ..
+mkdir samples
+cd samples
+
+git clone https://github.com/jhipster/jhipster-sample-app.git
+git clone https://github.com/jhipster/jhipster-sample-app-gradle.git
+git clone https://github.com/jhipster/jhipster-sample-app-java7.git
+git clone https://github.com/jhipster/jhipster-sample-app-gulp.git
+git clone https://github.com/jhipster/jhipster-sample-app-mongodb.git
+git clone https://github.com/jhipster/jhipster-sample-app-cassandra.git
+git clone https://github.com/jhipster/jhipster-sample-app-elasticsearch.git
+git clone https://github.com/jhipster/jhipster-sample-app-hazelcast.git
+git clone https://github.com/jhipster/jhipster-sample-app-nocache.git
+git clone https://github.com/jhipster/jhipster-sample-app-compass.git
+git clone https://github.com/jhipster/jhipster-sample-app-noi18n.git
+git clone https://github.com/jhipster/jhipster-sample-app-websocket.git
+git clone https://github.com/jhipster/jhipster-sample-app-oauth2.git
+git clone https://github.com/jhipster/jhipster-sample-app-token.git
