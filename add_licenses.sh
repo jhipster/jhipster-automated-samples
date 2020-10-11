a67cdfa7cb60b5be3ab201a8fd6deefd628da297
Pascal Grimaud <pgrimaud@ippon.fr>
2020-10-11 18:35:28 +0200

    Add scripts for adding licenses

---
+++ add_licenses.sh
@@ -0,0 +1,163 @@
+#!/bin/sh
+echo "Starting to add license to JHipster sample projects"
+cd ../samples
+
+echo "---------------------------------------------"
+echo "current project: jhipster-sample-app"
+echo "---------------------------------------------"
+cd jhipster-sample-app
+cp ../../jhipster-automated-samples/LICENSE.txt .
+git add .
+git commit -m "Add LICENSE"
+git push
+cd ..
+
+echo "---------------------------------------------"
+echo "current project: jhipster-sample-app-react"
+echo "---------------------------------------------"
+cd jhipster-sample-app-react
+cp ../../jhipster-automated-samples/LICENSE.txt .
+git add .
+git commit -m "Add LICENSE"
+git push
+cd ..
+
+echo "---------------------------------------------"
+echo "current project: jhipster-sample-app-gradle"
+echo "---------------------------------------------"
+cd jhipster-sample-app-gradle
+cp ../../jhipster-automated-samples/LICENSE.txt .
+git add .
+git commit -m "Add LICENSE"
+git push
+cd ..
+
+echo "---------------------------------------------"
+echo "current project: jhipster-sample-app-mongodb"
+echo "---------------------------------------------"
+cd jhipster-sample-app-mongodb
+cp ../../jhipster-automated-samples/LICENSE.txt .
+git add .
+git commit -m "Add LICENSE"
+git push
+cd ..
+
+echo "---------------------------------------------"
+echo "current project: jhipster-sample-app-couchbase"
+echo "---------------------------------------------"
+cd jhipster-sample-app-couchbase
+cp ../../jhipster-automated-samples/LICENSE.txt .
+git add .
+git commit -m "Add LICENSE"
+git push
+cd ..
+
+echo "---------------------------------------------"
+echo "current project: jhipster-sample-app-cassandra"
+echo "---------------------------------------------"
+cd jhipster-sample-app-cassandra
+cp ../../jhipster-automated-samples/LICENSE.txt .
+git add .
+git commit -m "Add LICENSE"
+git push
+cd ..
+
+echo "---------------------------------------------"
+echo "current project: jhipster-sample-app-couchbase"
+echo "---------------------------------------------"
+cd jhipster-sample-app-couchbase
+cp ../../jhipster-automated-samples/LICENSE.txt .
+git add .
+git commit -m "Add LICENSE"
+git push
+cd ..
+
+echo "---------------------------------------------"
+echo "current project: jhipster-sample-app-dto"
+echo "---------------------------------------------"
+cd jhipster-sample-app-dto
+cp ../../jhipster-automated-samples/LICENSE.txt .
+git add .
+git commit -m "Add LICENSE"
+git push
+cd ..
+
+echo "---------------------------------------------"
+echo "current project: jhipster-sample-app-elasticsearch"
+echo "---------------------------------------------"
+cd jhipster-sample-app-elasticsearch
+cp ../../jhipster-automated-samples/LICENSE.txt .
+git add .
+git commit -m "Add LICENSE"
+git push
+cd ..
+
+echo "---------------------------------------------"
+echo "current project: jhipster-sample-app-hazelcast"
+echo "---------------------------------------------"
+cd jhipster-sample-app-hazelcast
+cp ../../jhipster-automated-samples/LICENSE.txt .
+git add .
+git commit -m "Add LICENSE"
+git push
+cd ..
+
+echo "---------------------------------------------"
+echo "current project: jhipster-sample-app-nocache"
+echo "---------------------------------------------"
+cd jhipster-sample-app-nocache
+cp ../../jhipster-automated-samples/LICENSE.txt .
+git add .
+git commit -m "Add LICENSE"
+git push
+cd ..
+
+echo "---------------------------------------------"
+echo "current project: jhipster-sample-app-noi18n"
+echo "---------------------------------------------"
+cd jhipster-sample-app-noi18n
+cp ../../jhipster-automated-samples/LICENSE.txt .
+git add .
+git commit -m "Add LICENSE"
+git push
+cd ..
+
+echo "---------------------------------------------"
+echo "current project: jhipster-sample-app-websocket"
+echo "---------------------------------------------"
+cd jhipster-sample-app-websocket
+cp ../../jhipster-automated-samples/LICENSE.txt .
+git add .
+git commit -m "Add LICENSE"
+git push
+cd ..
+
+echo "---------------------------------------------"
+echo "current project: jhipster-sample-app-oauth2"
+echo "---------------------------------------------"
+cd jhipster-sample-app-oauth2
+cp ../../jhipster-automated-samples/LICENSE.txt .
+git add .
+git commit -m "Add LICENSE"
+git push
+cd ..
+
+echo "---------------------------------------------"
+echo "current project: jhipster-sample-app-gateway"
+echo "---------------------------------------------"
+cd jhipster-sample-app-gateway
+cp ../../jhipster-automated-samples/LICENSE.txt .
+git add .
+git commit -m "Add LICENSE"
+git push
+cd ..
+
+echo "---------------------------------------------"
+echo "current project: jhipster-sample-app-microservice"
+echo "---------------------------------------------"
+cd jhipster-sample-app-microservice
+cp ../../jhipster-automated-samples/LICENSE.txt .
+git add .
+git commit -m "Add LICENSE"
+git push
+cd ..
