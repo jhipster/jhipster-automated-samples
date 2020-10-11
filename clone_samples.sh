dc0fb8990637ef1d7c221ac8d0a893debbbe0a0f
Pascal Grimaud <pgrimaud@ippon.fr>
2020-10-11 17:59:39 +0200

    Clone with ssh instead of https

--- clone_samples.sh
+++ clone_samples.sh
@@ -4,20 +4,20 @@ cd ..
 mkdir samples
 cd samples
 
-git clone https://github.com/jhipster/jhipster-sample-app.git --single-branch --depth 1
-git clone https://github.com/jhipster/jhipster-sample-app-react.git --single-branch --depth 1
-git clone https://github.com/jhipster/jhipster-sample-app-gradle.git --single-branch --depth 1
-git clone https://github.com/jhipster/jhipster-sample-app-mongodb.git --single-branch --depth 1
-git clone https://github.com/jhipster/jhipster-sample-app-couchbase.git --single-branch --depth 1
-git clone https://github.com/jhipster/jhipster-sample-app-cassandra.git --single-branch --depth 1
-git clone https://github.com/jhipster/jhipster-sample-app-dto.git --single-branch --depth 1
-git clone https://github.com/jhipster/jhipster-sample-app-elasticsearch.git --single-branch --depth 1
-git clone https://github.com/jhipster/jhipster-sample-app-hazelcast.git --single-branch --depth 1
-git clone https://github.com/jhipster/jhipster-sample-app-nocache.git --single-branch --depth 1
-git clone https://github.com/jhipster/jhipster-sample-app-noi18n.git --single-branch --depth 1
-git clone https://github.com/jhipster/jhipster-sample-app-websocket.git --single-branch --depth 1
-git clone https://github.com/jhipster/jhipster-sample-app-oauth2.git --single-branch --depth 1
-git clone https://github.com/jhipster/jhipster-sample-app-gateway.git --single-branch --depth 1
-git clone https://github.com/jhipster/jhipster-sample-app-microservice.git --single-branch --depth 1
-git clone https://github.com/jhipster/jhipster-sample-app-vuejs.git --single-branch --depth 1
-git clone https://github.com/jhipster/jhipster-sample-app-kotlin.git --single-branch --depth 1
+git clone git@github.com:jhipster/jhipster-sample-app.git --single-branch --depth 1
+git clone git@github.com:jhipster/jhipster-sample-app-react.git --single-branch --depth 1
+git clone git@github.com:jhipster/jhipster-sample-app-gradle.git --single-branch --depth 1
+git clone git@github.com:jhipster/jhipster-sample-app-mongodb.git --single-branch --depth 1
+git clone git@github.com:jhipster/jhipster-sample-app-couchbase.git --single-branch --depth 1
+git clone git@github.com:jhipster/jhipster-sample-app-cassandra.git --single-branch --depth 1
+git clone git@github.com:jhipster/jhipster-sample-app-dto.git --single-branch --depth 1
+git clone git@github.com:jhipster/jhipster-sample-app-elasticsearch.git --single-branch --depth 1
+git clone git@github.com:jhipster/jhipster-sample-app-hazelcast.git --single-branch --depth 1
+git clone git@github.com:jhipster/jhipster-sample-app-nocache.git --single-branch --depth 1
+git clone git@github.com:jhipster/jhipster-sample-app-noi18n.git --single-branch --depth 1
+git clone git@github.com:jhipster/jhipster-sample-app-websocket.git --single-branch --depth 1
+git clone git@github.com:jhipster/jhipster-sample-app-oauth2.git --single-branch --depth 1
+git clone git@github.com:jhipster/jhipster-sample-app-gateway.git --single-branch --depth 1
+git clone git@github.com:jhipster/jhipster-sample-app-microservice.git --single-branch --depth 1
+git clone git@github.com:jhipster/jhipster-sample-app-vuejs.git --single-branch --depth 1
+git clone git@github.com:jhipster/jhipster-sample-app-kotlin.git --single-branch --depth 1
