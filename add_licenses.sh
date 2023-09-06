5133d8ced5e8742152e940bb499828d78b6dcf29
Deepu <d4udts@gmail.com>
2023-09-06 08:15:29 +0200

    update scripts

--- add_licenses.sh
+++ add_licenses.sh
@@ -2,172 +2,25 @@
 echo "Starting to add license to JHipster sample projects"
 cd ../samples
 
-echo "---------------------------------------------"
-echo "current project: jhipster-sample-app"
-echo "---------------------------------------------"
-cd jhipster-sample-app
-cp ../../jhipster-automated-samples/LICENSE.txt .
-git add .
-git commit -m "Add LICENSE"
-git push
-cd ..
+folders=("jhipster-sample-app" "jhipster-sample-app-cassandra" "jhipster-sample-app-couchbase" "jhipster-sample-app-dto" "jhipster-sample-app-elasticsearch" "jhipster-sample-app-gateway" "jhipster-sample-app-gradle" "jhipster-sample-app-hazelcast" "jhipster-sample-app-microservice" "jhipster-sample-app-mongodb" "jhipster-sample-app-nocache" "jhipster-sample-app-noi18n" "jhipster-sample-app-oauth2" "jhipster-sample-app-react" "jhipster-sample-app-vuejs" "jhipster-sample-app-websocket")
 
-echo "---------------------------------------------"
-echo "current project: jhipster-sample-app-react"
-echo "---------------------------------------------"
-cd jhipster-sample-app-react
-cp ../../jhipster-automated-samples/LICENSE.txt .
-git add .
-git commit -m "Add LICENSE"
-git push
-cd ..
+# Iterate through each project
+for folder in "${folders[@]}"; do
+        # Check if the folder exists
+    if [ -d "$folder" ]; then
+        echo "---------------------------------------------"
+        echo "current project: $folder"
+        echo "---------------------------------------------"
+        cd $folder
+        cp ../../jhipster-automated-samples/LICENSE.txt .
+        git add .
+        git commit -m "Add LICENSE"
+        git push
+        cd ..
+    else
+        echo "Folder '$folder' does not exist." && exit
+    fi
+done
 
-echo "---------------------------------------------"
-echo "current project: jhipster-sample-app-vuejs"
-echo "---------------------------------------------"
-cd jhipster-sample-app-vuejs
-cp ../../jhipster-automated-samples/LICENSE.txt .
-git add .
-git commit -m "Add LICENSE"
-git push
-cd ..
 
-echo "---------------------------------------------"
-echo "current project: jhipster-sample-app-gradle"
-echo "---------------------------------------------"
-cd jhipster-sample-app-gradle
-cp ../../jhipster-automated-samples/LICENSE.txt .
-git add .
-git commit -m "Add LICENSE"
-git push
-cd ..
 
-echo "---------------------------------------------"
-echo "current project: jhipster-sample-app-mongodb"
-echo "---------------------------------------------"
-cd jhipster-sample-app-mongodb
-cp ../../jhipster-automated-samples/LICENSE.txt .
-git add .
-git commit -m "Add LICENSE"
-git push
-cd ..
-
-echo "---------------------------------------------"
-echo "current project: jhipster-sample-app-couchbase"
-echo "---------------------------------------------"
-cd jhipster-sample-app-couchbase
-cp ../../jhipster-automated-samples/LICENSE.txt .
-git add .
-git commit -m "Add LICENSE"
-git push
-cd ..
-
-echo "---------------------------------------------"
-echo "current project: jhipster-sample-app-cassandra"
-echo "---------------------------------------------"
-cd jhipster-sample-app-cassandra
-cp ../../jhipster-automated-samples/LICENSE.txt .
-git add .
-git commit -m "Add LICENSE"
-git push
-cd ..
-
-echo "---------------------------------------------"
-echo "current project: jhipster-sample-app-couchbase"
-echo "---------------------------------------------"
-cd jhipster-sample-app-couchbase
-cp ../../jhipster-automated-samples/LICENSE.txt .
-git add .
-git commit -m "Add LICENSE"
-git push
-cd ..
-
-echo "---------------------------------------------"
-echo "current project: jhipster-sample-app-dto"
-echo "---------------------------------------------"
-cd jhipster-sample-app-dto
-cp ../../jhipster-automated-samples/LICENSE.txt .
-git add .
-git commit -m "Add LICENSE"
-git push
-cd ..
-
-echo "---------------------------------------------"
-echo "current project: jhipster-sample-app-elasticsearch"
-echo "---------------------------------------------"
-cd jhipster-sample-app-elasticsearch
-cp ../../jhipster-automated-samples/LICENSE.txt .
-git add .
-git commit -m "Add LICENSE"
-git push
-cd ..
-
-echo "---------------------------------------------"
-echo "current project: jhipster-sample-app-hazelcast"
-echo "---------------------------------------------"
-cd jhipster-sample-app-hazelcast
-cp ../../jhipster-automated-samples/LICENSE.txt .
-git add .
-git commit -m "Add LICENSE"
-git push
-cd ..
-
-echo "---------------------------------------------"
-echo "current project: jhipster-sample-app-nocache"
-echo "---------------------------------------------"
-cd jhipster-sample-app-nocache
-cp ../../jhipster-automated-samples/LICENSE.txt .
-git add .
-git commit -m "Add LICENSE"
-git push
-cd ..
-
-echo "---------------------------------------------"
-echo "current project: jhipster-sample-app-noi18n"
-echo "---------------------------------------------"
-cd jhipster-sample-app-noi18n
-cp ../../jhipster-automated-samples/LICENSE.txt .
-git add .
-git commit -m "Add LICENSE"
-git push
-cd ..
-
-echo "---------------------------------------------"
-echo "current project: jhipster-sample-app-websocket"
-echo "---------------------------------------------"
-cd jhipster-sample-app-websocket
-cp ../../jhipster-automated-samples/LICENSE.txt .
-git add .
-git commit -m "Add LICENSE"
-git push
-cd ..
-
-echo "---------------------------------------------"
-echo "current project: jhipster-sample-app-oauth2"
-echo "---------------------------------------------"
-cd jhipster-sample-app-oauth2
-cp ../../jhipster-automated-samples/LICENSE.txt .
-git add .
-git commit -m "Add LICENSE"
-git push
-cd ..
-
-echo "---------------------------------------------"
-echo "current project: jhipster-sample-app-gateway"
-echo "---------------------------------------------"
-cd jhipster-sample-app-gateway
-cp ../../jhipster-automated-samples/LICENSE.txt .
-git add .
-git commit -m "Add LICENSE"
-git push
-cd ..
-
-echo "---------------------------------------------"
-echo "current project: jhipster-sample-app-microservice"
-echo "---------------------------------------------"
-cd jhipster-sample-app-microservice
-cp ../../jhipster-automated-samples/LICENSE.txt .
-git add .
-git commit -m "Add LICENSE"
-git push
-cd ..
--- clone_samples.sh
+++ clone_samples.sh
@@ -5,20 +5,11 @@ mkdir samples
 cd samples
 
 GIT_URL="https://github.com/jhipster" # or git@github.com:jhipster
+# projects
+projects=("jhipster-sample-app" "jhipster-sample-app-cassandra" "jhipster-sample-app-couchbase" "jhipster-sample-app-dto" "jhipster-sample-app-elasticsearch" "jhipster-sample-app-gateway" "jhipster-sample-app-gradle" "jhipster-sample-app-hazelcast" "jhipster-sample-app-microservice" "jhipster-sample-app-mongodb" "jhipster-sample-app-nocache" "jhipster-sample-app-noi18n" "jhipster-sample-app-oauth2" "jhipster-sample-app-react" "jhipster-sample-app-vuejs" "jhipster-sample-app-websocket")
 
-git clone $GIT_URL/jhipster-sample-app.git --single-branch --depth 1 || exit
-git clone $GIT_URL/jhipster-sample-app-react.git --single-branch --depth 1 || exit
-git clone $GIT_URL/jhipster-sample-app-vuejs.git --single-branch --depth 1 || exit
-git clone $GIT_URL/jhipster-sample-app-gradle.git --single-branch --depth 1 || exit
-git clone $GIT_URL/jhipster-sample-app-mongodb.git --single-branch --depth 1 || exit
-git clone $GIT_URL/jhipster-sample-app-couchbase.git --single-branch --depth 1 || exit
-git clone $GIT_URL/jhipster-sample-app-cassandra.git --single-branch --depth 1 || exit
-git clone $GIT_URL/jhipster-sample-app-dto.git --single-branch --depth 1 || exit
-git clone $GIT_URL/jhipster-sample-app-elasticsearch.git --single-branch --depth 1 || exit
-git clone $GIT_URL/jhipster-sample-app-hazelcast.git --single-branch --depth 1 || exit
-git clone $GIT_URL/jhipster-sample-app-nocache.git --single-branch --depth 1 || exit
-git clone $GIT_URL/jhipster-sample-app-noi18n.git --single-branch --depth 1 || exit
-git clone $GIT_URL/jhipster-sample-app-websocket.git --single-branch --depth 1 || exit
-git clone $GIT_URL/jhipster-sample-app-oauth2.git --single-branch --depth 1 || exit
-git clone $GIT_URL/jhipster-sample-app-gateway.git --single-branch --depth 1 || exit
-git clone $GIT_URL/jhipster-sample-app-microservice.git --single-branch --depth 1 || exit
+# Iterate through each project
+for project in "${projects[@]}"; do
+    git clone $GIT_URL/$project.git --single-branch --depth 1 || exit
+done
+
