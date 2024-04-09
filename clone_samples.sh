db87ee64f450229b78a773b79850a08a914d9ee2
Matt Raible <matt@raibledesigns.com>
2024-04-09 08:01:43 -0400

    Change GIT_URL to git@ since password no longer supported

--- clone_samples.sh
+++ clone_samples.sh
@@ -4,7 +4,7 @@ cd ..
 mkdir samples
 cd samples
 
-GIT_URL="https://github.com/jhipster" # or git@github.com:jhipster
+GIT_URL="git@github.com:jhipster"
 # projects
 projects=("jhipster-sample-app" "jhipster-sample-app-cassandra" "jhipster-sample-app-couchbase" "jhipster-sample-app-dto" "jhipster-sample-app-elasticsearch" "jhipster-sample-app-gateway" "jhipster-sample-app-gradle" "jhipster-sample-app-hazelcast" "jhipster-sample-app-microservice" "jhipster-sample-app-mongodb" "jhipster-sample-app-nocache" "jhipster-sample-app-noi18n" "jhipster-sample-app-oauth2" "jhipster-sample-app-react" "jhipster-sample-app-vuejs" "jhipster-sample-app-websocket")
 
